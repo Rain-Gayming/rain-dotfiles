0e0ea83f7719f78032f05a9769787ca70bd2b692
rain <raingayming@gmail.com>
2024-09-20 15:39:41 +0100

    nvim

---
+++ config_hypr/nvim/lua/plugins/chadrc.lua
@@ -0,0 +1,5 @@
+
+local M = {}
+M.ui = {theme = 'Tokyonight'}
+M.plugins = 'custom.plugins'
+return M
---
+++ config_hypr/nvim/lua/plugins/init.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "wilder",
+      },
+    },
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4',
+    lazy = false,
+    ["rust-analyzer"] = {
+      cargo = {
+        AllFeatures = true,
+      },
+    },
+  },
+  {
+    'gelguy/wilder.nvim',
+  },
+  {
+    'xiyaowong/transparent.nvim',
+
+  }
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
